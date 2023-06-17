78eb0560965797ff71c3bbe2c2898093091027f0
Umair Ali Khalid <umair.ali.khalid@viaplaygroup.com>
2023-06-17 11:22:49 +0200

    Preview devices

--- app/src/main/java/com/techlads/composetvtemplate/MainActivity.kt
+++ app/src/main/java/com/techlads/composetvtemplate/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.tv.material3.MaterialTheme
@@ -60,7 +61,7 @@ fun GDGGreetings() {
 }
 
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, device = Devices.TV_1080p)
 @Composable
 fun GreetingPreview() {
     ComposeTvTemplateTheme {
